f5b80bea328d673c6142edfd9dd479456ec14e24
aarabhib <aarabhibala@gmail.com>
2022-10-29 14:18:31 -0400

    adding window and bg img

--- pingpong.py
+++ pingpong.py
@@ -1,1 +1,21 @@
-import pygame
+from pygame import *
+
+clock = time.Clock()
+FPS = 60
+
+game = True
+
+window = display.set_mode((700,500))
+display.set_caption('Ping Pong Game')
+background = transform.scale(image.load("galaxy.jpg"), (700,500))
+
+
+while game:
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+
+    window.blit(background, (0,0))
+
+    display.update()
+    clock.tick(FPS)
